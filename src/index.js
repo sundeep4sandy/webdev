51c46fabdef3ef9627d3fa63af0dea34d8a31fae
Sandeep Yerramsetti <yerramsetti.sandeep15@gmail.com>
2019-04-04 19:30:46 -0700

    JSX

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = function () {
-  return <div> Hello </div>;
+// Create a react component
+const App = () => {
+  const buttonText = { text: 'Click me' };
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'green', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App/>, document.querySelector('#root'));
