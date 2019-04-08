e5be50be4827047b9cb24077153fc3e4e4c4ef41
Sandeep Yerramsetti <yerramsetti.sandeep15@gmail.com>
2019-04-08 16:38:30 -0700

    new

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import ApprovalCard from './ApprovalCard';
-import CommentDetail from './CommentDetail';
 
-// Create a react component
-const App = function ()  {
-
-  return (<div className="ui container comments">
-  <ApprovalCard>
-<CommentDetail
-  author="sandeep"
-   timeago="yesterday"
-  image = {faker.image.avatar()}
-content = "this is good"
-/>
-</ApprovalCard>
-  <ApprovalCard>
-<CommentDetail author="sadhika" timeago="tmrw" image = {faker.image.avatar()}
-content = "this is awesome" /></ApprovalCard>
-<ApprovalCard>
-<CommentDetail author="sandy" timeago="today" image = {faker.image.avatar()}
-content = "this is bes"/></ApprovalCard>
-
-  </div>);
+const App = function () {
+  return <div> Hello </div>;
 };
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
