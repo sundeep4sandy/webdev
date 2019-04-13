cd77d9b3e7a0e144ca78ed4a232484f6e5a4e0ee
Sandeep Yerramsetti <yerramsetti.sandeep15@gmail.com>
2019-04-13 11:31:17 -0700

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import SongList from './SongList';
+import SongDetail from './SongDetail';
 
-class App extends React.Component
-{
-  render()
-  {
-    return (
-    <div className="ui container" style={{ marginTop: '10px' }} >
-    hi
-     </div>
-   );
-  }
-}
+const App = function () {
+      return (
+        <div className="ui container grid">
+          <div className="ui row">
+            <div className="column eight wide">
+              <SongList />
+            </div>
+            <div className="column eight wide">
+              <SongDetail />
+            </div>
+          </div>
+        </div>
+      );
+    };
+
 export default App;
---
+++ src/components/SongDetail.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions';
+
+const SongDetail = function ({ song }) {
+  console.log(song);
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+      Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = function (state) {
+  return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
