f1517765806bc837a3ed30c2560402ce3000328e
Sandeep Yerramsetti <yerramsetti.sandeep15@gmail.com>
2019-04-13 09:38:11 -0700

    delete

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-      'Client-ID 65fceaa665b24cd86accda3881bdfad40862e327d6194f27d50fa8817a9382c5',
-  },
-});
