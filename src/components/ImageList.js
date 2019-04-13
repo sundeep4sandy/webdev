b6c58efdb81b3e5a28505eeabfc39d099feaf3bb
Sandeep Yerramsetti <yerramsetti.sandeep15@gmail.com>
2019-04-12 20:20:42 -0700

    merge to master

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import ImageCard from './ImageCard.js';
+import './ImageList.css';
 
 const ImageList = function (props) {
     //console.log(props.images);
     const images = props.images.map(
       function (image) {
-          return <img src={image.urls.regular} key={image.id} alt={image.description}/>;
+          return < ImageCard key={image.id} image ={image}/>;
         });
 
-    return <div> {images}</div>;
+    return <div  className="image-list"> {images}</div>;
   };
 
 export default ImageList;
